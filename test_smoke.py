7a231f34fd6d54825dfa075a0c6fdd2ca4224536
George333222 <samula483@gmail.com>
2026-02-01 19:40:06 +0530

    update 4

--- test_smoke.py
+++ test_smoke.py
@@ -1,12 +1,12 @@
 import pytest
 
 @pytest.mark.smoke
-def test_example():
+def test_example_1():
     assert True
 
 
 
 
 @pytest.mark.smoke
-def test_example():
+def test_example_2():
     assert 1 == 1
