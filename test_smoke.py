a835f7875614d4ada44ce86eb8552159d12aab8e
George333222 <samula483@gmail.com>
2026-02-01 20:23:25 +0530

    up 0.2

--- test_smoke.py
+++ test_smoke.py
@@ -1,12 +1,13 @@
+# new_demo/tests/test_sample.py
 import pytest
 
 @pytest.mark.smoke
-def test_example_1():
-    assert True
-
-
-
+def test_addition():
+    assert 1 + 1 == 2
 
 @pytest.mark.smoke
-def test_example_2():
-    assert 1 == 1
+def test_subtraction():
+    assert 5 - 3 == 2
+
+def test_not_smoke():
+    assert 10 * 2 == 20
